44e074ee135f4e5fc5331325ca010f8aab953540
Brenda Lopes <20058225@mydbs.ie>
2025-01-27 16:18:06 +0000

    up js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,21 @@
+// Load cards
 document.addEventListener("DOMContentLoaded", function () {
   const charactersList = document.querySelector(".charactersList");
-
   fetch("src/data/cards.json")
     .then((response) => {
-      if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`);}
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
       return response.json();
     })
     .then((cardsData) => {
+      // Add cards to the DOM
       charactersList.innerHTML = cardsData
         .map((card, index) => createCardHTML(card, index === 0))
         .join("");
+
+      // Initialize card navigation logic AFTER the cards are loaded
+      initializeCardNavigation();
     })
     .catch((error) => console.error("Error fetching card data:", error));
 
@@ -18,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
       .fill('<span class="star"></span>')
       .join("");
     return `
-      <li class="card ${card.id} ${isSelected ? "select" : ""}">
+      <li class="card ${card.id} ${isSelected ? "selected" : ""}">
         <div class="cardBack"></div>
         <h2 class="name">${card.name}</h2>
         <div class="cardLevel">${stars}</div>
@@ -33,4 +39,32 @@ document.addEventListener("DOMContentLoaded", function () {
       </li>
     `;
   }
+
+  function initializeCardNavigation() {
+    const btnAdvance = document.getElementById("btnAdvance");
+    const btnGoBack = document.getElementById("btnGoBack");
+    const cards = document.querySelectorAll(".card");
+    let cardCurrent = 0;
+
+    btnAdvance.addEventListener("click", function () {
+      if(cardCurrent === cards.length - 1) return;      
+      hideSelectedCard();
+      cardCurrent ++; 
+      showCard(cards, cardCurrent);
+    });
+    btnGoBack.addEventListener("click", function () {
+      if(cardCurrent === 0) return;
+      hideSelectedCard();
+      cardCurrent --; 
+      showCard(cards, cardCurrent);
+    });
+  }
 });
+function showCard(cards, cardCurrent) {
+  cards[cardCurrent].classList.add("selected");
+}
+function hideSelectedCard() {
+  const selectedCard = document.querySelector(".selected");
+  selectedCard.classList.remove("selected");
+}
+
